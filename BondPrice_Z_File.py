6e1421083d0e45dc2b5547fbb8d2a3e4a278e844
yf2781-droid <yf2781@columbia.edu>
2026-02-21 22:43:05 -0500

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,16 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    coupon = face * couponRate
+    bondPrice = 0.0
+
+    for t, y in zip(times, yc):
+        if t != times[-1]:
+            cf = coupon
+        else:
+            cf = coupon + face
+
+        pv = cf / ((1 + y) ** t)
+        bondPrice += pv
+
+    return bondPrice
