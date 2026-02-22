6129cfb41b80316295431776cb8a9ba1a0f95826
yf2781-droid <yf2781@columbia.edu>
2026-02-21 22:23:38 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,16 @@
 
 
-def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+def getBondPrice_E(face, couponRate, m, yc):
+    coupon = face * couponRate
+    bondPrice = 0.0
+
+    for i, y in enumerate(yc, start=1):  
+        if i < m:
+            cf = coupon
+        else:
+            cf = coupon + face
+
+        pv = cf / ((1 + y) ** i)
+        bondPrice += pv
+
+    return bondPrice
